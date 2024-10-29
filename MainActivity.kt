d9e28a2904e3966e704cb49e8f42486bef0a4894
n.v.l.p <140810282+linhphongz@users.noreply.github.com>
2024-10-29 22:53:04 +0700

    bai1

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-
+ 
 class MainActivity : AppCompatActivity() {
     private lateinit var editText1: EditText
     private lateinit var editText2: EditText
